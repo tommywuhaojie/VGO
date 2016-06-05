7b24fc395e47a1bb24f5b31f587b15b2608fe9e0
CindyLingruixin <cindyling1994@hotmail.com>
2016-06-04 22:13:30 -0700

    login in UI done

--- app/src/test/java/Register_Tests.java
+++ app/src/test/java/Register_Tests.java
@@ -92,19 +92,5 @@ public class Register_Tests {
         }
     }
 
-    @Test
-    public void Register_login_logout_example_tests() throws  Exception
-    {
-        JSONObject result1 = User.Register("7786889380", "1165637488@qq.com", "aA123456", "Tommy", "Wu");
-        System.out.println("-->Register Result: " + result1.toString());
-        Assert.assertNotEquals("1", result1.getString("code"));
 
-        JSONObject result2 = User.Login("7786889380", "aA123456");
-        System.out.println("-->Login Result: " + result2.toString());
-        Assert.assertEquals("1", result2.getString("code"));
-
-        JSONObject result3 = User.Logout();
-        System.out.println("-->Logout Result: " + result3.toString());
-        Assert.assertEquals("1", result3.getString("code"));
-    }
 }
